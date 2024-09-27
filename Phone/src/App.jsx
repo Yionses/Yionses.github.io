114e5b08214362838a9adab505ac4a344e7e487b
Yionse <v-zhangtianc@microsoft.com>
2024-09-27 14:50:21 +0800

    :bug:

--- Phone/src/App.jsx
+++ Phone/src/App.jsx
@@ -1,6 +1,6 @@
 import { lazy, useState } from "react"
 import "./App.css"
-import { Button, Input, Select } from "antd"
+import { Button, Input, message, Select, Space } from "antd"
 import datas from "../datas"
 
 function App() {
@@ -9,55 +9,91 @@ function App() {
   const [point, setPoint] = useState("")
   const [frameRate, setFrameRate] = useState("")
   const [heating, setHeating] = useState("")
+  const [isOpen, setIsOpen] = useState(false)
+  const [pass, setPass] = useState("")
   return (
-    <div
-      style={{
-        width: "700px",
-        backgroundColor: "gray",
-        padding: "20px",
-      }}
-    >
-      <div
-        className="top"
-        style={{
-          boxSizing: "border-box",
-          paddingLeft: "20px",
-        }}
-      >
-        <Button>查询</Button>
-        <Input style={{ width: "200px", marginLeft: "20px" }} value={heating} />
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          paddingLeft: "20px",
-          marginTop: "20px",
-        }}
-      >
-        <Select
-          style={{ width: "200px" }}
-          showSearch
-          options={datas.map((item) => ({
-            label: item.model,
-            value: item.model,
-          }))}
-          onChange={(value) => {
-            const res = datas.find((item) => item.model === value)
-            setCpu("CPU：" + res.cpu)
-            setPoint("画质：" + res.point)
-            setFrameRate("帧率：" + res.frameRate)
-            setHeating("发烫程度：" + res.heating)
+    <>
+      {isOpen ? (
+        <div
+          style={{
+            width: "700px",
+            backgroundColor: "red",
+            padding: "20px",
           }}
-        ></Select>
-        <Input style={{ width: "200px", marginLeft: "20px" }} value={cpu} />
-        <Input style={{ width: "200px", marginLeft: "20px" }} value={point} />
-        <Input
-          style={{ width: "200px", marginLeft: "20px" }}
-          value={frameRate}
-        />
-      </div>
-    </div>
+        >
+          <div
+            className="top"
+            style={{
+              boxSizing: "border-box",
+              paddingLeft: "20px",
+            }}
+          >
+            <button
+              style={{ background: "green", width: "80px", color: "white" }}
+            >
+              查询
+            </button>
+            <Input
+              style={{ width: "200px", marginLeft: "20px" }}
+              value={heating}
+            />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              paddingLeft: "20px",
+              marginTop: "20px",
+            }}
+          >
+            <Select
+              style={{ width: "200px" }}
+              showSearch
+              options={datas.map((item) => ({
+                label: item.model,
+                value: item.model,
+              }))}
+              onChange={(value) => {
+                const res = datas.find((item) => item.model === value)
+                setCpu("CPU：" + res.cpu)
+                setPoint("画质：" + res.point)
+                setFrameRate("帧率：" + res.frameRate)
+                setHeating("发烫程度：" + res.heating)
+              }}
+            ></Select>
+            <Input style={{ width: "200px", marginLeft: "20px" }} value={cpu} />
+            <Input
+              style={{ width: "200px", marginLeft: "20px" }}
+              value={point}
+            />
+            <Input
+              style={{ width: "200px", marginLeft: "20px" }}
+              value={frameRate}
+            />
+          </div>
+        </div>
+      ) : (
+        <Space>
+          <Input
+            value={pass}
+            type="password"
+            onChange={(e) => setPass(e.target.value)}
+          ></Input>
+          <Button
+            onClick={() => {
+              if (pass === "GuaGua") {
+                setIsOpen(true)
+                setPass("")
+              } else {
+                message.error("密码错误")
+              }
+            }}
+          >
+            查询
+          </Button>
+        </Space>
+      )}
+    </>
   )
 }
 
