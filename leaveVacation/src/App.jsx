32d45e50976c3f92f965ab969134f6bda13ff206
Tiancheng Zhang <2458015575@qq.com>
2024-11-06 23:50:09 +0800

    :sparkles: 假期模拟

---
+++ leaveVacation/src/App.jsx
@@ -0,0 +1,15 @@
+import imgs from "../imgs/logo.png"
+
+function App() {
+  return (
+    <img
+      src={imgs}
+      style={{
+        width: "4rem",
+        height: "4rem",
+      }}
+    ></img>
+  )
+}
+
+export default App
