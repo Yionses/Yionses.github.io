c3fa8964d3b2ae23b92b0caed54d13c30c228bc9
Yionse <2458015575@qq.com>
2025-04-26 01:23:59 +0800

    H5支付

---
+++ community/dist/index.js
@@ -0,0 +1,26 @@
+;(function (doc, win) {
+  let docEl = doc.documentElement, // 获取html对象
+    resizeEvt = "orientationchange" in window ? "orientationchange" : "resize", // 横屏是否支持，不支持则为浏览器大小改变
+    // 计算页面字体大小
+    recalc = function () {
+      // 获取页面宽度
+      let clientWidth = docEl.clientWidth
+      // 获取不到页面宽度，直接返回
+      if (!clientWidth) {
+        return
+      }
+      let size = clientWidth / 7.5
+      console.log(size > 100 ? 100 : size)
+
+      // 设置html字体大小(浏览器默认字体大小为16px)
+      docEl.style.fontSize = (size > 100 ? 100 : size) + "px"
+    }
+  // 不支持addEventListener,返回
+  if (!doc.addEventListener) {
+    return
+  }
+  // 监听事件，获取当前html标签的字体大小
+  win.addEventListener(resizeEvt, recalc, false)
+  // dom内容加在完成事件
+  doc.addEventListener("DOMContentLoaded", recalc, false)
+})(document, window)
